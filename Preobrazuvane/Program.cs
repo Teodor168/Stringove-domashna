fff1086a541b1bf4a41085102eda9ab3883fbc6f
Teodor168 <teodor16iliev@gmail.com>
2024-03-19 20:17:55 +0200

    neshto

---
+++ Preobrazuvane/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers.Text;
+
+namespace Preobrazuvane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            int baseNumber = int.Parse(Console.ReadLine()); 
+            int num = int.Parse(Console.ReadLine());
+
+            //Action
+            string result = "";
+            while (num > 0)
+            {
+                int remainder = num % baseNumber;
+                result = remainder.ToString() + result;
+                num /= baseNumber;
+            }
+
+            //Print
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Sravnqvane/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Sravnqvane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi neshto: ");
+            string firstInput = Console.ReadLine().ToLower();
+            Console.Write("Vuvedi neshto: ");
+            string secondInput = Console.ReadLine().ToLower();
+
+            int comparisonResult = string.Compare(firstInput, secondInput);
+
+            if (comparisonResult < 0)
+            {
+                Console.WriteLine("firstInput > secondInput");
+            }
+            else if (comparisonResult > 0)
+            {
+                Console.WriteLine("secondInput > firstInput");
+            }
+            else
+            {
+                Console.WriteLine("equal");
+            }
+        }
+    }
+}
